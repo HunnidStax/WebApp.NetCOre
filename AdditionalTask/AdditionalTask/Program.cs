60d432d8a26277b7b4912484b4afcd8cb4d63920
HunnidStax <den4ik.trofimenko@gmail.com>
2022-04-11 22:47:01 +0300

    Done

--- AdditionalTask/AdditionalTask/Program.cs
+++ AdditionalTask/AdditionalTask/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AdditionalTask
 {
@@ -6,7 +8,25 @@ namespace AdditionalTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string str = "   Предложение  один    Теперь  предложение   два     Предложение    три    ";
+            Console.WriteLine(str);
+
+            RegexOptions options = RegexOptions.None;
+            Regex regex = new Regex("[ ]{2,}", options);
+            string res = regex.Replace(str, " ");
+
+            Console.WriteLine(res);
+
+            StringBuilder stringBuilder = new StringBuilder(res);
+
+            for (int i = 2; i < res.Length; i++)
+            {
+                if (res[i] >= 'A' && res[i] <= 'Я')
+                {
+                    stringBuilder[i - 1] = '.';
+                }
+            }
+            Console.WriteLine(stringBuilder);
         }
     }
 }
